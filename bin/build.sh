13ac14824c577f8f08795c69cd24e838f1fd90a8
CodeLionX <sebastian.schmidl@t-online.de>
2018-08-03 15:43:00 +0200

    added build script

---
+++ bin/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+mainFile=$1
+pdfName=$1.pdf
+[ -z "$1" ] && pdfName=$2
+
+echo "Checking file existance for ${pdfName}"
+# remove pdfs if present
+[ -f "${pdfName}" ] && rm "${pdfName}" && echo "removed already existing PDF"
+
+# test if pdf still exists
+[ -f "${pdfName}" ] && echo "PDF is still there! Exiting test!" && exit 1
+
+
+# build pdf
+echo "Building PDF using pdflatex and biber on ${mainFile}.tex"
+pdflatex -synctex=1 -interaction=nonstopmode "${mainFile}.tex"
+biber "${mainFile}"
+pdflatex -synctex=1 -interaction=nonstopmode "${mainFile}.tex"
+
+# only exit successfully if pdf was build successfully
+[ -f "${pdfName}" ] && exit 0 || exit 1
