05cd77a0718f82afdc1db99ecc4e76f25fc583b5
CodeLionX <sebastian.schmidl@t-online.de>
2018-08-03 18:07:12 +0200

    wip

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 mainFile=$1
-if [ -z $2 ]; then
-  pdfName=$2
-else
-  pdfName=$1.pdf
-fi
+pdfName=$1.pdf
+
 echo "Setting pdf name to ${pdfName}"
 
 echo "Checking file existance for ${pdfName}"
@@ -21,7 +18,6 @@ echo "Building PDF using pdflatex and biber on ${mainFile}.tex"
 pdflatex -synctex=1 -interaction=nonstopmode -shell-escape "${mainFile}.tex"
 biber "${mainFile}"
 pdflatex -synctex=1 -interaction=nonstopmode -shell-escape "${mainFile}.tex"
-mv "${mainFile}.pdf" "${pdfName}"
 
 # only exit successfully if pdf was build successfully
 [ -f "${pdfName}" ] && exit 0 || exit 1
