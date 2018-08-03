be1900a967ad1217696263eb71391df9ee5b7ec3
CodeLionX <sebastian.schmidl@t-online.de>
2018-08-03 20:00:57 +0200

    added github access token and made git calls quiet

--- bin/travis-push.sh
+++ bin/travis-push.sh
@@ -9,11 +9,12 @@ git config --global user.name "Travis CI"
 
 git checkout ${TRAVIS_BRANCH}
 git add -f "${pdfFile}"
-git commit -m "Travis update of PDF: ${pdfFile} on $(date) (Build ${TRAVIS_BUILD_NUMBER})" -m "[skip ci]"
+git commit -m "Travis update of PDF: ${pdfFile} on $(date) (Build ${TRAVIS_BUILD_NUMBER})" -m "[skip ci]" >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
   echo "A new PDF file exists. Uploading it to GitHub"
-  git push origin ${TRAVIS_BRANCH}
+  git remote add github "https://${GH_TOKEN}@github.com/CodeLionX/actordb-paper.git" >/dev/null 2>&1
+  git push --quiet --set-upstream github ${TRAVIS_BRANCH}
 else
   echo "No changes. Skipping upload"
 fi
