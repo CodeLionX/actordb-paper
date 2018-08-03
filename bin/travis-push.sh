ed117b1a3ac900a2d65a220d005308f17189dde4
CodeLionX <sebastian.schmidl@t-online.de>
2018-08-03 19:27:45 +0200

    wip

--- bin/travis-push.sh
+++ bin/travis-push.sh
@@ -7,10 +7,11 @@ echo "Trying to push PDF to GitHub"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git checkout ${TRAVIS_BRANCH}
 git add -f "${pdfFile}"
-git commit -m "Travis update of PDF: ${pdfFile}\n $(date) (Build ${TRAVIS_BUILD_NUMBER})" -m "[skip ci]"
+git commit -m "Travis update of PDF: ${pdfFile} on $(date) (Build ${TRAVIS_BUILD_NUMBER})" -m "[skip ci]"
 
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo "A new PDF file exists. Uploading it to GitHub"
   git push origin master
 else
